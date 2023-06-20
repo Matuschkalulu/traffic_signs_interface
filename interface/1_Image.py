f9e06948b0be9b3621e154faa7c0e7aa14d416af
Madhu Jatheendran <mjatheendran@gmail.com>
2023-06-20 15:42:44 +0100

    Changes for API for Multi Image detection

--- interface/1_Image.py
+++ interface/1_Image.py
@@ -47,16 +47,17 @@ if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         files = {"file": image_data}
         print (files)
-        response = requests.post("https://trafficsignscode-ugznwmrhlq-ew.a.run.app/ImagePrediction/", files=files)
-        #response = requests.post("http://127.0.0.1:8000/ImagePrediction", files=files)
+        #response = requests.post("https://trafficsignscode-ugznwmrhlq-ew.a.run.app/ImagePrediction/", files=files)
+        response = requests.post("http://127.0.0.1:8000/ImagePrediction", files=files)
         print(response)
         if response.status_code == 200:
             # the prediction result
             prediction = response.json()
-            if prediction['Value'] >= 0.4:
+            #print(prediction)
+            if prediction['Actual Prediction Value'][0] >= 0.4:
                 st.error('This is an unreadable!') #, icon=f"\N:rotating_light:")
                 print(prediction)
-            if prediction['Value'] < 0.4:
+            if prediction['Actual Prediction Value'][0] < 0.4:
                 st.success('This is a readable!') #icon=f"\N:white_check_mark:")
         else:
             st.error("Failed to classify the image. Please try again.")
