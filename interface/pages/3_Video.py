aaa0a6d9885d53adcab67ec579431d9189f17a68
Madhu Jatheendran <mjatheendran@gmail.com>
2023-06-21 18:23:50 +0100

    Video.py FINALLY working

--- interface/pages/3_Video.py
+++ interface/pages/3_Video.py
@@ -5,28 +5,6 @@ import os
 import base64
 import subprocess
 import io
-<<<<<<< HEAD
-from ffmpeg import FFmpeg, Progress
-#Comment for Git
-
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('png-clipart-marvel-avengers-illustration-avengers-group-close-up-comics-and-fantasy-various-comics-thumbnail.png')
-=======
-
-
 #def add_bg_from_local(image_file):
    # with open(image_file, "rb") as image_file:
         #encoded_string = base64.b64encode(image_file.read())
@@ -42,7 +20,7 @@ add_bg_from_local('png-clipart-marvel-avengers-illustration-avengers-group-close
     #unsafe_allow_html=True
     #)
 #add_bg_from_local('png-clipart-marvel-avengers-illustration-avengers-group-close-up-comics-and-fantasy-various-comics-thumbnail.png')
->>>>>>> main
+
 
 # for the Streamlit interface
 st.title("Traffic Sign Recognition")
@@ -57,11 +35,9 @@ def local_css(file_name):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Loading CSS
-<<<<<<< HEAD
-url_css = os.path.join(os.getcwd(), 'pages', 'frontend', 'css', 'streamlit.css')
-=======
+
 url_css = os.path.join(os.getcwd(), 'interface','pages', 'frontend', 'css', 'streamlit.css')
->>>>>>> main
+
 local_css(url_css)
 
 def remote_css(url):
@@ -76,18 +52,6 @@ st.markdown('<style>...</style>', unsafe_allow_html=True)
 
 if uploaded_file is not None:
     # displaying the uploaded image
-<<<<<<< HEAD
-    #video_file = open(uploaded_file, 'rb')
-    #video_bytes = uploaded_file.read()
-    #st.video(video_bytes)
-    g = io.BytesIO(uploaded_file.read())  ## BytesIO Object
-    temporary_location = "testout_simple.mp4"
-    #print (g)
-    #subprocess.run(['ffmpeg', '-i', 'testout_simple.mp4', '-c:v', 'libx264', '-preset', 'slow', '-crf', '22', '-c:a', 'copy', 'testout_simple.mp4'])
-    with open(temporary_location, 'wb') as out:  ## Open temporary file as bytes
-        out.write(g.read())  ## Read bytes into file
-    f= open('testout_simple.mp4', 'rb')
-=======
 
     g = io.BytesIO(uploaded_file.read())  ## BytesIO Object
     temporary_location = "interface/testout_simple.mp4"
@@ -95,7 +59,6 @@ if uploaded_file is not None:
     with open(temporary_location, 'wb') as out:  ## Open temporary file as bytes
         out.write(g.read())  ## Read bytes into file
     f= open('interface/testout_1.mp4', 'rb')
->>>>>>> main
     video_bytes = f.read()
     st.video(video_bytes)
 
@@ -103,41 +66,14 @@ if uploaded_file is not None:
 
     if st.button("Classify"):
         # Prepare the image data
-<<<<<<< HEAD
-        video_data = uploaded_file.read()
-        video_file_buffer = uploaded_file
-        if video_file_buffer is not None:
-            col1, col2 = st.columns(2)
-        with col1:
-            st.video(video_file_buffer)
-        with col2:
-            with st.spinner("Labeling signs ..."):
-                # img_bytes = img_file_buffer.getvalue()
-                video_data=uploaded_file.getvalue()
-                files={'video': video_data}
-                res = requests.post("http://127.0.0.1:8000/VideoPrediction", files=files)
-                video_path = os.path.join(os.getcwd(),'outputFromStreamlit.mp4')
-                if os.path.exists(video_path):
-                    os.remove(video_path)
-
-                if res.status_code == 200:
-                    video_bytes = res.content
-                    with open('myvideo.mp4', 'wb') as f:
-                        f.write(video_bytes)
-                    subprocess.run(['ffmpeg', '-i', 'myvideo.mp4', '-c:v', 'libx264', '-preset', 'slow', '-crf', '22', '-c:a', 'copy', 'outputFromStreamlit.mp4'])
-                    with open('outputFromStreamlit.mp4', 'rb') as f:
-                        video_bytes = f.read()
-                    st.video(video_bytes)
-                else:
-                    st.markdown("**Oops**, something went wrong :sweat: Please try again.")
-                    print(res.status_code, res.content)
-=======
+        print(uploaded_file)
         video_data=uploaded_file.getvalue()
-        files={'video': uploaded_file}
-        res = requests.post("http://127.0.0.1:8000/VideoPrediction/", files=files)
-        video_path = os.path.join(os.getcwd(),'outputFromStreamlit.mp4')
-        if os.path.exists(video_path):
-            os.remove(video_path)
+        files={'file': open('interface/testout_1.mp4', 'rb')}
+        #res = requests.post("http://127.0.0.1:8000/VideoPrediction", files = files)
+        res = requests.post("http://localhost:8000/VideoPrediction", files = files)
+        #video_path = os.path.join(os.getcwd(),'outputFromStreamlit.mp4')
+        #if os.path.exists(video_path):
+            #os.remove(video_path)
 
         if res.status_code == 200:
             video_bytes = res.content
@@ -150,4 +86,3 @@ if uploaded_file is not None:
         else:
             st.markdown("**Oops**, something went wrong :sweat: Please try again.")
             print(res.status_code, res.content)
->>>>>>> main
