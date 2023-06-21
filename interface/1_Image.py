95f49cab6c3715bf609eee79ea8ad218a1235f82
Ludwig Matuschka <131038284+Matuschkalulu@users.noreply.github.com>
2023-06-21 12:43:47 +0200

    Update 1_Image.py

--- interface/1_Image.py
+++ interface/1_Image.py
@@ -35,30 +35,30 @@ st.markdown('<style>...</style>', unsafe_allow_html=True)
 
 with st.columns(2)[0]:
 
-if uploaded_file is not None:
-    # displaying the uploaded image
-    image = Image.open(uploaded_file)
-    print(image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-    #pred_image = Image.open(io.BytesIO(image_bytes))
-
-    # making a prediction
-    if st.button("Classify"):
-        # Prepare the image data
-        image_data = uploaded_file.getvalue()
-        files = {"file": image_data}
-        print (files)
-        #response = requests.post("https://trafficsignscode-ugznwmrhlq-ew.a.run.app/ImagePrediction/", files=files)
-        response = requests.post("http://127.0.0.1:8000/ImagePrediction", files=files)
-        print(response)
-        if response.status_code == 200:
-            # the prediction result
-            prediction = response.json()
-            #print(prediction)
-            if prediction['Actual Prediction Value'][0] >= 0.4:
-                st.error('This is an unreadable!') #, icon=f"\N:rotating_light:")
-                print(prediction)
-            if prediction['Actual Prediction Value'][0] < 0.4:
-                st.success('This is a readable!') #icon=f"\N:white_check_mark:")
-        else:
-            st.error("Failed to classify the image. Please try again.")
+    if uploaded_file is not None:
+        # displaying the uploaded image
+        image = Image.open(uploaded_file)
+        print(image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        #pred_image = Image.open(io.BytesIO(image_bytes))
+    
+        # making a prediction
+        if st.button("Classify"):
+            # Prepare the image data
+            image_data = uploaded_file.getvalue()
+            files = {"file": image_data}
+            print (files)
+            #response = requests.post("https://trafficsignscode-ugznwmrhlq-ew.a.run.app/ImagePrediction/", files=files)
+            response = requests.post("http://127.0.0.1:8000/ImagePrediction", files=files)
+            print(response)
+            if response.status_code == 200:
+                # the prediction result
+                prediction = response.json()
+                #print(prediction)
+                if prediction['Actual Prediction Value'][0] >= 0.4:
+                    st.error('This is an unreadable!') #, icon=f"\N:rotating_light:")
+                    print(prediction)
+                if prediction['Actual Prediction Value'][0] < 0.4:
+                    st.success('This is a readable!') #icon=f"\N:white_check_mark:")
+            else:
+                st.error("Failed to classify the image. Please try again.")
