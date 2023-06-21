4bf653f082033d0ab659209e54684455e875c5cd
Mohamed Aly <mohamedscience7@gmail.com>
2023-06-21 13:51:12 +0200

    Starting working on the street images

--- interface/pages/2_ImageClassification.py
+++ interface/pages/2_ImageClassification.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import os
+from PIL import Image
 
 st.title("Traffic Sign Recognition")
 st.write("Classification on images")
@@ -14,7 +15,37 @@ def local_css(file_name):
 def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
 
-# Loading CSS
-url_css = os.path.join(os.getcwd(), 'pages', 'frontend', 'css', 'streamlit.css')
-local_css(url_css)
-remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+    # Loading CSS
+    url_css = os.path.join(os.getcwd(), 'pages', 'frontend', 'css', 'streamlit.css')
+    local_css(url_css)
+    remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+st.markdown('<style>...</style>', unsafe_allow_html=True)
+
+if uploaded_file is not None:
+    # displaying the uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    #pred_image = Image.open(io.BytesIO(image_bytes))
+
+    # making a prediction
+    if st.button("Classify"):
+        # Prepare the image data
+        image_data = uploaded_file.getvalue()
+        files = {"file": image_data}
+        print (files)
+        response = requests.post("https://trafficsignscode-ugznwmrhlq-ew.a.run.app/ImagePrediction/", files=files)
+        #response = requests.post("http://127.0.0.1:8000/ImagePrediction", files=files)
+        print(response)
+        if response.status_code == 200:
+            # the prediction result
+            prediction = response.json()
+            if prediction['Value'] > 0.6:
+                st.error('This is an unreadable!') #, icon=f"\N:rotating_light:")
+                print(prediction)
+            else:
+                st.success('This is a readable!') #icon=f"\N:white_check_mark:")
+        else:
+            st.error("Failed to classify the image. Please try again.")
