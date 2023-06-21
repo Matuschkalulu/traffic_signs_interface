b7317535990cc4ede314b0ec5a3fb58e0dea5f7e
Ludwig Matuschka <ludwigmatuschka@gmail.com>
2023-06-21 22:29:45 +0200

    Changes

--- interface/1_Image.py
+++ interface/1_Image.py
@@ -2,7 +2,6 @@ import streamlit as st
 import requests
 from PIL import Image
 import os
-import io
 from io import BytesIO
 import numpy as np
 
@@ -29,35 +28,4 @@ def remote_css(url):
     local_css("frontend/css/streamlit.css")
     remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-st.markdown('<style>...</style>', unsafe_allow_html=True)
-
-with st.columns(2)[0]:
-    if uploaded_file is not None:
-        # displaying the uploaded image
-        image = Image.open(uploaded_file)
-        print(image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
-        #pred_image = Image.open(io.BytesIO(image_bytes))
-
-        # making a prediction
-        if st.button("Classify"):
-            # Prepare the image data
-            image_data = uploaded_file.getvalue()
-            files = {"file": image_data}
-            print (files)
-            #response = requests.post("https://trafficsignscode-ugznwmrhlq-ew.a.run.app/ImagePrediction/", files=files)
-            response = requests.post("http://127.0.0.1:8000/ImagePrediction", files=files)
-            print(response)
-            if response.status_code == 200:
-                # the prediction result
-                prediction = response.json()
-                #print(prediction)
-                if prediction['Actual Prediction Value'][0] >= 0.4:
-                    st.error('This is an unreadable!') #, icon=f"\N:rotating_light:")
-                    print(prediction)
-                if prediction['Actual Prediction Value'][0] < 0.4:
-                    st.success('This is a readable!') #icon=f"\N:white_check_mark:")
-            else:
-                st.error("Failed to classify the image. Please try again.")
+st.image('https://cdn-icons-png.flaticon.com/512/2555/2555555.png')
